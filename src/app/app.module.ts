67479781dce38a33033d5cb8b3ebc19e9e048647
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-12-13 11:44:43 +0900

    feat: 30. Adding & Configuring the Submit Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }                from '@angular/core';
 import { BrowserModule }           from '@angular/platform-browser';
+import { FormsModule }             from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule }        from '@angular/flex-layout';
 
@@ -30,7 +31,8 @@ import { AppRoutingModule }         from './app-routing.module';
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
