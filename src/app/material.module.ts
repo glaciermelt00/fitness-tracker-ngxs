9b9cbb5220b4f0a7ac9888b4b47c453754720249
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-12-19 22:34:59 +0900

    feat: 49. Adding a Spinner to the Training Screen

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule }     from "@angular/material/button";
-import { MatIconModule }       from '@angular/material/icon';
-import { MatFormFieldModule }  from '@angular/material/form-field';
-import { MatInputModule }      from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatCheckboxModule }   from "@angular/material/checkbox";
-import { MatSidenavModule }    from "@angular/material/sidenav"
-import { MatToolbarModule }    from "@angular/material/toolbar"
-import { MatListModule }       from "@angular/material/list"
-import { MatTabsModule }       from "@angular/material/tabs"
-import { MatCardModule }       from "@angular/material/card"
-import { MatSelectModule }       from "@angular/material/select"
+import { MatButtonModule }          from "@angular/material/button";
+import { MatIconModule }            from '@angular/material/icon';
+import { MatFormFieldModule }       from '@angular/material/form-field';
+import { MatInputModule }           from '@angular/material/input';
+import { MatDatepickerModule }      from '@angular/material/datepicker';
+import { MatNativeDateModule }      from "@angular/material/core";
+import { MatCheckboxModule }        from "@angular/material/checkbox";
+import { MatSidenavModule }         from "@angular/material/sidenav"
+import { MatToolbarModule }         from "@angular/material/toolbar"
+import { MatListModule }            from "@angular/material/list"
+import { MatTabsModule }            from "@angular/material/tabs"
+import { MatCardModule }            from "@angular/material/card"
+import { MatSelectModule }          from "@angular/material/select"
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
 
 const MODULES = [
   MatButtonModule,
@@ -27,7 +28,8 @@ const MODULES = [
   MatListModule,
   MatTabsModule,
   MatCardModule,
-  MatSelectModule
+  MatSelectModule,
+  MatProgressSpinnerModule,
 ]
 
 @NgModule({
