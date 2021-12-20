720143e32308c8b49daf834ec3a5d7ca7d34b62b
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-12-20 10:22:07 +0900

    style: align text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { WelcomeComponent }         from './welcome/welcome.component';
 import { AppRoutingModule }         from './app-routing.module';
 import { HeaderComponent }          from './navigation/header/header.component';
 import { SidenavListComponent }     from './navigation/sidenav-list/sidenav-list.component';
-import { StopTrainingComponent } from './training/current-training/stop-training.component';
+import { StopTrainingComponent }    from './training/current-training/stop-training.component';
 
 @NgModule({
   declarations: [
