8a4ef9a090cd466e97e2860b424fae6498b959b6
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-12-22 09:50:40 +0900

    feat: 66. Controlling the Active Exercise

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { TrainingService } from './training.service';
+
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(
+      exercise => {
+        if (exercise) {
+          this.ongoingTraining = true;
+        } else {
+          this.ongoingTraining = false;
+        }
+      }
+    );
   }
 
 }
