dca5c82a880cc726480dbd0a45c07cc61547f1b9
glaciermelt00 <glacier-melt@hotmail.co.jp>
2021-12-23 09:08:06 +0900

    feat: 70. Handling 'Complete' and 'Cancel' Events

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
 
@@ -11,7 +11,6 @@ import { TrainingService }       from '../training.service';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
@@ -29,6 +28,7 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = window.setInterval(() => {
       this.progress = this.progress + 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
     }, step_ms)
@@ -44,7 +44,7 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress);
       } else {
         this.startOrResumeTimer();
       }
