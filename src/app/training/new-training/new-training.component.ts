5b77860c4aca6764b12975ffa77d8161906ac4c6
glaciermelt00 <glacier-melt@hotmail.co.jp>
2022-01-01 22:03:17 +0900

    feat: 104. Improving Error Handling

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -34,6 +34,10 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
         this.exercises = exercises;
       }
     );
+    this.fetchExercises();
+  }
+
+  fetchExercises() {
     this.trainingService.fetchAvailableExercises();
   }
 
