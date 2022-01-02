2345874ce3e7a9b58359a918a7154445e72e2ac5
glaciermelt00 <glacier-melt@hotmail.co.jp>
2022-01-02 23:00:45 +0900

    feat: 109. Loading a Module Lazily

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
-import { NgModule }               from "@angular/core";
-import { RouterModule, Routes }   from "@angular/router";
+import { NgModule }             from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { WelcomeComponent }  from './welcome/welcome.component';
-import { TrainingComponent } from './training/training.component';
-import { AuthGuard }         from './auth/auth.guard';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthGuard }        from "./auth/auth.guard";
 
 const routes: Routes = [
-  { path: '',          component: WelcomeComponent },
-  { path: 'training',  component: TrainingComponent, canActivate: [AuthGuard] },
+  { path: '',         component: WelcomeComponent },
+  { path: 'training', loadChildren: () => import('./training/training.module').then(m => m.TrainingModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,26 @@ import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule }        from '@angular/flex-layout';
 import { AngularFireModule }       from '@angular/fire/compat';
+import { AngularFirestoreModule }  from '@angular/fire/compat/firestore';
 
-import { AppComponent }          from './app.component';
-import { MaterialModule }        from './material.module';
-import { WelcomeComponent }      from './welcome/welcome.component';
-import { AppRoutingModule }      from './app-routing.module';
-import { HeaderComponent }       from './navigation/header/header.component';
-import { SidenavListComponent }  from './navigation/sidenav-list/sidenav-list.component';
-import { AuthService }           from './auth/auth.service';
-import { TrainingService }       from './training/training.service';
-import { environment }           from '../environments/environment';
-import { UIService }             from './shared/ui.service';
-import { AuthModule }            from './auth/auth.module';
-import { TrainingModule }        from './training/training.module';
+import { AppComponent }         from './app.component';
+import { MaterialModule }       from './material.module';
+import { WelcomeComponent }     from './welcome/welcome.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { HeaderComponent }      from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { AuthService }          from './auth/auth.service';
+import { TrainingService }      from './training/training.service';
+import { environment }          from '../environments/environment';
+import { UIService }            from './shared/ui.service';
+import { AuthModule }           from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +32,7 @@ import { TrainingModule }        from './training/training.module';
     FlexLayoutModule,
     AngularFireModule.initializeApp(environment.firebase),
     AuthModule,
-    TrainingModule,
+    AngularFirestoreModule
   ],
   providers: [
     AuthService,
