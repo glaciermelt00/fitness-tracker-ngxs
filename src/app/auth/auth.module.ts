49aaba362d84bea0f1ad6127223b28f842cfeb6e
glaciermelt00 <glacier-melt@hotmail.co.jp>
2022-01-02 21:47:05 +0900

    feat: 106. Optimizing Subscriptions

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,9 @@
-import { NgModule }            from "@angular/core";
-import { CommonModule }        from "@angular/common";
-import { FormsModule }         from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
-import { FlexLayoutModule }    from '@angular/flex-layout';
+import { NgModule }              from "@angular/core";
+import { CommonModule }          from "@angular/common";
+import { FormsModule }           from "@angular/forms";
+import { ReactiveFormsModule }   from "@angular/forms";
+import { FlexLayoutModule }      from '@angular/flex-layout';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent }  from './login/login.component';
@@ -19,6 +20,7 @@ import { MaterialModule }  from '../material.module';
     ReactiveFormsModule,
     MaterialModule,
     FlexLayoutModule,
+    AngularFireAuthModule,
   ]
 })
 export class AuthModule { }
