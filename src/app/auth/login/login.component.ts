cd1cdd792ac035a5fc1cafa9652c70c832ad796b
glaciermelt00 <glacier-melt@hotmail.co.jp>
2022-01-03 23:14:53 +0900

    style: align text

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }       from '@angular/core';
+import { Component, OnInit }                  from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store }                              from '@ngrx/store';
 
@@ -7,7 +7,7 @@ import { map }                      from 'rxjs/operators';
 
 import { AuthService } from '../auth.service';
 import { UIService }   from '../../shared/ui.service';
-import * as fromApp from '../../app.reducer';
+import * as fromApp    from '../../app.reducer';
 
 @Component({
   selector: 'app-login',
