414244a85194c4cf39baf304c6529fe71c62cd2b
glaciermelt00 <glacier-melt@hotmail.co.jp>
2022-01-03 21:29:08 +0900

    feat: 111. Wrap Up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDsVpqkERUVpYD8YgeajMSJt_JhWARUYQ",
+    authDomain: "ng-fitness-tracker-b57fc.firebaseapp.com",
+    projectId: "ng-fitness-tracker-b57fc",
+    storageBucket: "ng-fitness-tracker-b57fc.appspot.com",
+    messagingSenderId: "825537643850",
+    appId: "1:825537643850:web:c93ff63939ab575d236db3"
+  }
 };
